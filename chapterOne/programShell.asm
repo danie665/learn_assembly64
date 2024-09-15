6eadc2276aa044dbc88965919c0302846bde204f
Taylor Danielson <taylor.t.danielson@outlook.com>
2024-09-15 13:55:00 -0700

    I was wrong about how thing should be labeled

--- chapterOne/programShell.asm
+++ chapterOne/programShell.asm
@@ -13,7 +13,9 @@
 section	.text
 	global _start	; Declare the entry point
 
-_start:			; Entry point label
+_start:	
+
+	; Exit system call
 	mov rax, 60	; syscall number of sys_exit in 64-bit Linux
 	xor rdi, rdi	; exit code 0
 	syscall		; make the syscall to exit the program

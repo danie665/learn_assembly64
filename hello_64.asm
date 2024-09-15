8fba41407fce7e8bc935503828e1df336d4c4f8a
Taylor Danielson <taylor.t.danielson@outlook.com>
2024-09-15 02:38:43 -0700

    first push

---
+++ hello_64.asm
@@ -0,0 +1,7 @@
+; hello_64.asm	print a string using printf
+; Assemble:	nasm -f elf64 -l hello_64.lst hello_64.asm
+; Link:		gcc -m64 -o hello_64 hello_64.o
+; Run: 		./hello_64 > hello_64.out
+; Output:	cat hello_64.out
+
+
